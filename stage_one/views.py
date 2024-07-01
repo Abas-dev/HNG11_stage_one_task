4b6f7a633c808abeea3dc3e4767ba618f8b6b43a
Abasifreke Isaac <79318104+Abas-dev@users.noreply.github.com>
2024-07-01 14:14:43 +0100

    working on weather

--- stage_one/views.py
+++ stage_one/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 import requests
+from django.contrib.gis.geoip2 import GeoIP2
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -10,13 +11,17 @@ def get_client_ip(request):
     return ip
 
 def get_location(request):
+
+    g = GeoIP2(get_client_ip)
+    log_lat = g.lon_lat()
     ip_address = get_client_ip(request)
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
     location_data = {
         "ip": ip_address,
         "city": response.get("city"),
         "region": response.get("region"),
-        "country": response.get("country_name")
+        "country": response.get("country_name"),
+        "lat_lat": log_lat
     }
     return JsonResponse(location_data)
 
