96a7645f7b64fce74c80ef9af9bec12bf46f4150
Abasifreke Isaac <79318104+Abas-dev@users.noreply.github.com>
2024-07-01 13:41:18 +0100

    getting ip address

---
+++ stage_one/views.py
@@ -0,0 +1,23 @@
+from django.http import JsonResponse
+import requests
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def get_location(request):
+    ip_address = get_client_ip(request)
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name")
+    }
+    return JsonResponse(location_data)
+
+# In your urls.py, you would then connect this view to a URL.
